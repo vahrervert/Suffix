789bb91538aa14f5d81938294d91f9ea03202e74
moondog <78167549+itsmoondog@users.noreply.github.com>
2021-10-24 10:27:00 +0300

    Delete src directory

--- src/dev/moondog/suffix/Stats.java
+++
@@ -1,8 +0,0 @@
-package dev.moondog.suffix;
-
-public class Stats {
-         public static String SUFFIX_VERSION = "1.0.0";
-         public static String SUFFIX_NAME = "Suffix";
-         public static final Boolean BETA = false;
-         public static final Boolean DEV = false;
-}
--- src/dev/moondog/suffix/util/EnabledManager.java
+++
@@ -1,7 +0,0 @@
-package dev.moondog.suffix.util;
-
-public class EnabledManager {
-    public static void isEnabled() {
-
-    }
-}
